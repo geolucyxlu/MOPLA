167eb84381b83e9e08e7432b8dd5cd020cc19c95
Lucy Lu <32547740+MOPLA@users.noreply.github.com>
2022-11-23 12:36:55 +0000

    Delete 2phase_frictional_viscous_model directory

--- 2phase_frictional_viscous_model/routines/contract1.m
+++
@@ -1,6 +0,0 @@
-function m = contract1(x,y)
-% contract1.m
-% Double-index contraction between two 2nd-order tensors.
-%--------------------------------------------------------------------------
-    m = sum(sum(x.*y));
-end
